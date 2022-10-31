eccf93af8cb901002553672388f5b8444c2c96b7
Zulfiqor <faridun.hofiz@gmail.com>
2022-10-31 18:49:43 +0100

    index.html

--- rockps.js
+++ rockps.js
@@ -13,38 +13,60 @@ function getComputerChoice(){
 playerScore = 0;
 computerScore = 0;
 
-function playRound() {
-    for (let i = 0; i < 5; i++) {
-        const playerSelection = prompt("Rock, Paper or Scissors?").toUpperCase();
-        const computerSelection = getComputerChoice();
-        if (playerSelection == computerSelection) {
-            //return ("Draw! You both chose same");
-            continue;
-        }
-        else if ((playerSelection == "ROCK" && computerSelection == "SCISSORS") || 
+function playRound(a) {
+    //for (let i = 0; i < 5; i++) {
+        //const  playerSelection = prompt("Rock, Paper or Scissors?").toUpperCase();
+        let playerSelection = a;
+        let computerSelection = getComputerChoice();
+        // if (playerSelection == computerSelection) {
+        //     //return ("Draw! You both chose same");
+        //     continue;
+        // }
+        if ((playerSelection == "ROCK" && computerSelection == "SCISSORS") || 
         (playerSelection == "SCISSORS" && computerSelection == "PAPER") || 
         (playerSelection == "PAPER" && computerSelection == "ROCK")) {
             //return ("You Win! " + playerSelection + " beats " + computerSelection);
             playerScore ++;
         }
-        else{
+        else if (((playerSelection == "SCISSORS" && computerSelection == "ROCK") || 
+        (playerSelection == "PAPER" && computerSelection == "SCISSORS") || 
+        (playerSelection == "ROCK" && computerSelection == "PAPER"))) {
             //return ("You Lose. " + computerSelection + " beats " + playerSelection);
             computerScore++;
         }
-        console.log("Computer " + computerSelection);
-        console.log("Player " + playerSelection)
+        console.log("Computer choice " + computerSelection);
+        resultsComputer.textContent = ("Comp Score: " + computerScore + " Computer choice " + computerSelection);
+        console.log("Your choice " + playerSelection);
+        resultsPlayer.textContent = ("Your Score: " + playerScore + " Your choice " + playerSelection);
+        console.log(resultsPlayer);
+    //}
+    if (playerScore == 5) {
+        alert("You win")
     }
-    if (playerScore == computerScore) {
-        return ("Draw")
-    }
-    else if (playerScore > computerScore) {
-        return ("You win by score of " + playerScore + " to " + computerScore);
-    }
-    else {
-        return ("You lose by score of " + computerScore + " to " + playerScore);
+    else if (computerScore == 5) {
+        alert("Computer wins");
     }
 }
 let playerSelection;
 let computerSelection;
-console.log("player" + playerSelection)
-console.log(playRound(playerSelection, computerSelection))
+//console.log("player" + playerSelection)
+//console.log(playRound(playerSelection, computerSelection))
+
+// to show results on html
+const resultsContainer = document.createElement('div');
+const resultsComputer = document.createElement('p');
+const resultsPlayer = document.createElement('p');
+const gameResults = document.createElement('p');
+resultsContainer.appendChild(resultsComputer);
+resultsContainer.appendChild(resultsPlayer);
+resultsContainer.appendChild(gameResults);
+document.body.appendChild(resultsContainer);
+
+
+const buttons = document.querySelectorAll(".playerChoice");
+buttons.forEach((button) => {
+    button.addEventListener('click', (e) => {
+        playRound(e.target.value);
+    });
+});
+
